e40f09b7bceae01a5ad74a24892530afed7ef278
Abhilasha kumari <abhilashakumari455@gmail.com>
2020-07-30 21:48:33 +0530

    Updates

---
+++ precache-manifest.b56b60b8939a0787ec33bc6d449e9545.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34cc398c36ff46218b7430e1ef359c6a",
+    "url": "/reactritu/index.html"
+  },
+  {
+    "revision": "be5452197880095cad68",
+    "url": "/reactritu/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f917be81001b19265e58",
+    "url": "/reactritu/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "be5452197880095cad68",
+    "url": "/reactritu/static/js/2.ee5685cb.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactritu/static/js/2.ee5685cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f917be81001b19265e58",
+    "url": "/reactritu/static/js/main.43b57269.chunk.js"
+  },
+  {
+    "revision": "fe2043186d554b523d94",
+    "url": "/reactritu/static/js/runtime-main.f9e11add.js"
+  }
+]);
